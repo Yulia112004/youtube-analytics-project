27551a4a88e818529b55b81e994a119a5d4128d1
Yulia Byckova <byckovaulia669@gmail.com>
2023-12-01 21:24:51 +0300

    Homework_6 Done

--- src/playlist.py
+++ src/playlist.py
@@ -1,125 +1,53 @@
 import datetime
-import isodate
+import os
 from googleapiclient.discovery import build
+import isodate
 from src.constants import API_KEY
-from src.video import Video
+
 
 class PlayList:
-    """
-    Represents a YouTube playlist.
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
 
-    Attributes:
-        playlist_id (str): The ID of the playlist.
-        __youtube (googleapiclient.discovery.Resource): The YouTube API resource object.
-        _pl_title (str): The title of the playlist.
-        _pl_url (str): The URL of the playlist.
-        _pl_video_durations (list): The durations of the videos in the playlist.
-    """
-
-    __youtube = build('youtube', 'v3', developerKey=API_KEY)
-
-    def __init__(self, playlist_id):
-        """
-        Initializes a PlayList object.
-
-        Args:
-            playlist_id (str): The ID of the playlist.
-        """
-        youtube_url = 'https://www.youtube.com/playlist?list='
+    def __init__(self, playlist_id: str) -> None:
+        self.video_url = None
         self.playlist_id = playlist_id
-        self._pl_title = self.playlist['snippet']['title']
-        self._pl_url = youtube_url + self.playlist_id
-        self._pl_video_durations = self._video_duration
+        self.playlist_videos = self.youtube.playlistItems().list(playlistId=self.playlist_id,
+                                                                 part='snippet, contentDetails, id, status',
+                                                                 maxResults=50).execute()
+        self.channel_id = self.playlist_videos['items'][0]['snippet']['channelId']
+        self.playlists = self.youtube.playlists().list(part='snippet', channelId=self.channel_id,
+                                                       maxResults=50).execute()
+
+        for item in self.playlists['items']:
+            if item['id'] == self.playlist_id:
+                self.title = item['snippet']['title']
+                break
+
+        self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
+        self.video_response = self.youtube.videos().list(part='contentDetails',
+                                                         id=','.join(self.video_ids)
+                                                         ).execute()
+        self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
 
     @property
     def total_duration(self):
-        """
-        Calculates the total duration of the playlist.
-
-        Returns:
-            datetime.timedelta: The total duration of the playlist.
-        """
-        return sum(self._video_duration, datetime.timedelta())
-
-    @property
-    def _playlist_video_ids(self):
-        """
-        Retrieves the video IDs of the playlist.
-
-        Returns:
-            list: A list of video IDs in the playlist.
-        """
-        playlist_content = self.__youtube.playlistItems().list(
-            playlistId=self.playlist_id,
-            part='contentDetails',
-        ).execute()
-        video_ids = [
-            video['contentDetails']['videoId']
-            for video in playlist_content['items']
-        ]
-        return video_ids
-
-    @property
-    def _video_duration(self):
-        """
-        Retrieves the durations of the videos in the playlist.
-
-        Returns:
-            list: A list of durations of the videos in the playlist.
-        """
-        videos = []
-        for video in self._playlist_video_ids:
-            videos.append(isodate.parse_duration(Video(video).duration))
-        return videos
-
-    @property
-    def playlist(self):
-        """
-        Retrieves the playlist details.
-
-        Returns:
-            dict: A dictionary containing the details of the playlist.
-        """
-        result = self.__youtube.playlists().list(
-            part='snippet',
-            id=self.playlist_id
-        ).execute()
-        return result['items'][0]
-
-    @property
-    def title(self):
-        """
-        Retrieves the title of the playlist.
-
-        Returns:
-            str: The title of the playlist.
-        """
-        return self._pl_title
-
-    @property
-    def url(self):
-        """
-        Retrieves the URL of the playlist.
-
-        Returns:
-            str: The URL of the playlist.
-        """
-        return self._pl_url
+        total_time_videos = datetime.timedelta()
+        for video in self.video_response['items']:
+            iso_8601_duration = video['contentDetails']['duration']
+            duration = isodate.parse_duration(iso_8601_duration)
+            total_time_videos += duration
+        return total_time_videos
 
     def show_best_video(self):
-        """
-        Finds the video with the most likes in the playlist.
+        like_count = 0
+        for video in self.video_response['items']:
+            video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                        id=video['id']).execute()
+            if int(video_response['items'][0]['statistics']['likeCount']) > like_count:
+                like_count = int(video_response['items'][0]['statistics']['likeCount'])
+                self.video_url = video_response['items'][0]['id']
 
-        Returns:
-            str: The URL of the video with the most likes in the playlist.
-        """
-        youtube_link = 'https://youtu.be/'
-        result_url = ''
-        most_liked = 0
-        for video in self._playlist_video_ids:
-            video_likes = Video(video).likes
-            if video_likes > most_liked:
-                most_liked = video_likes
-                result_url = youtube_link + video
+        return f'https://youtu.be/{self.video_url}'
 
-        return result_url
+    def __repr__(self):
+        return f'{self.playlist_videos}'
--- src/video.py
+++ src/video.py
@@ -1,172 +1,45 @@
-"""Video and Play List Video class module"""
-from googleapiclient.discovery import build  # pylint: disable=E0401
-
+import os
+from googleapiclient.discovery import build
 from src.constants import API_KEY
+
+class MyException(Exception):
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else "Hесуществующий id видео"
+
+    def __str__(self):
+        return self.message
 
 
 class Video:
-    """
-    Represents a YouTube video.
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
 
-    Attributes:
-        _video_id (str): The ID of the video.
-        _video_title (str): The title of the video.
-        _video_url (str): The URL of the video.
-        _video_views (str): The number of views of the video.
-        _video_likes (str): The number of likes on the video.
-    """
+    def __init__(self, video_id: str, ):
+        self.video_id = video_id
+        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                         id=video_id).execute()
+        try:
+            if len(self.video_response['items']) == 0:
+                raise MyException
 
-    __youtube = build(
-        'youtube',
-        'v3',
-        developerKey=API_KEY
-    )
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.url_video = f"https://youtu.be/{self.video_id}"
+            self.viewCount = self.video_response['items'][0]['statistics']['viewCount']
+            self.likeCount = self.video_response['items'][0]['statistics']['likeCount']
 
-    def __init__(self, video_id: str):
-        """
-        Initializes a Video object.
+        except MyException:
+            self.title = None
+            self.url_video = None
+            self.viewCount = None
+            self.likeCount = None
 
-        Args:
-            video_id (str): The ID of the video.
-        """
-        youtube_url = 'https://www.youtube.com/watch?v='
-        self._video_id = video_id
-        self._video_title = self._snippet['title']
-        self._video_url = youtube_url + video_id
-        self._video_views = self._statistics['viewCount']
-        self._video_likes = int(self._statistics['likeCount'])
-        self._video_duration = self._content_details['duration']
-
-    @property
-    def _video_data(self) -> dict:
-        """
-        Retrieves the video data from the YouTube API.
-
-        Returns:
-            dict: The video data.
-        """
-        result: dict = self.__youtube.videos().list(
-            part='snippet,statistics,contentDetails',
-            id=self._video_id
-        ).execute()
-        return result['items'][0]
-
-    @property
-    def _snippet(self) -> dict:
-        """
-        Retrieves the snippet data of the video.
-
-        Returns:
-            dict: The snippet data of the video.
-        """
-        return self._video_data['snippet']
-
-    @property
-    def _statistics(self) -> dict:
-        """
-        Retrieves the statistics data of the video.
-
-        Returns:
-            dict: The statistics data of the video.
-        """
-        return self._video_data['statistics']
-
-    @property
-    def _content_details(self):
-        """
-        Returns the content details of the video.
-
-        Returns:
-            dict: A dictionary containing the content details of the video.
-        """
-        return self._video_data['contentDetails']
-
-    @property
-    def duration(self):
-        """
-        Returns the duration of the video.
-
-        Returns:
-            int: The duration of the video in seconds.
-        """
-        return self._video_duration
-
-    @property
-    def title(self):
-        """
-        Returns the title of the video.
-
-        Returns:
-            str: The title of the video.
-        """
-        return self._video_title
-
-    @property
-    def url(self):
-        """
-        Returns the URL of the video.
-
-        Returns:
-            str: The URL of the video.
-        """
-        return self._video_url
-
-    @property
-    def views(self):
-        """
-        Returns the number of views of the video.
-
-        Returns:
-            int: The number of views of the video.
-        """
-        return self._video_views
-
-    @property
-    def likes(self):
-        """
-        Returns the number of likes of the video.
-
-        Returns:
-            int: The number of likes of the video.
-        """
-        return self._video_likes
-
-    def __str__(self) -> str:
-        """
-        Returns the string representation of the Video object.
-
-        Returns:
-            str: The title of the video.
-        """
-        return self._video_title
-
-    def __repr__(self):
-        """
-        Return a string representation of the Video object.
-
-        Returns:
-            str: A string representation of the Video object.
-        """
-        return self._video_id
+    def __str__(self):
+        return f'{self.title}'
 
 
 class PLVideo(Video):
-    """
-    Represents a video within a playlist.
-
-    Inherits from the Video class.
-
-    Attributes:
-        playlist_id (str): The ID of the playlist.
-    """
-
     def __init__(self, video_id: str, playlist_id: str):
-        """
-        Initializes a PLVideo object.
-
-        Args:
-            video_id (str): The ID of the video.
-            playlist_id (str): The ID of the playlist.
-        """
         super().__init__(video_id)
         self.playlist_id = playlist_id
+
+    def __str__(self):
+        return f'{self.title}'
